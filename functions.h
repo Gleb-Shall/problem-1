023291699cc84ec22c21405c5da81486a3906e73
Andrey Isakov <73563875+AndreyIsakov01@users.noreply.github.com>
2024-09-29 07:17:04 -0700

    Comments for functions.h

--- functions.h
+++ functions.h
@@ -1,8 +1,11 @@
+// Поменять название файла, чтобы соответствовало названию файла, где определяется функция
+// https://guiquanz.gitbooks.io/google-cc-style-guide/content/ebook/Naming.html
+
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <string>
 
-using namespace std;
+using namespace std;  // Плохая практика, лучше не привыкать использовать, чтобы не засорялось пространство имен. Перепиши все через std::
 
 void SayHello(string WordToSay);
 
